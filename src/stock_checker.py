239a4b519782fd38d8a0b6b4cc2385b143d32f32
Rohit Pradhan <rohit.p.pradhan88@gmail.com>
2020-12-23 18:14:56 -0500

    Added multithreading to page fetching

--- src/stock_checker.py
+++ src/stock_checker.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 import tldextract
 import os 
 from fake_useragent import UserAgent
+import concurrent.futures
+import multiprocessing
 
 os.chdir("..")
 soundpath = os.getcwd() + "\\assets\\instock.mp3"
@@ -16,6 +18,9 @@ jsonfp = os.getcwd() + "\websites\\"
 user_agent = UserAgent()
 browser_header = {'User-Agent': user_agent.chrome}
 
+# TODO:
+    # Add amazon to supported list
+    # Either improve CLI or create a GUI
 
 # Take in an array of strings, find which retailer they are from, check based on the corresponding JSON.
 def check_stock(links : list, sleep_time : float) -> None:
@@ -35,38 +40,43 @@ def check_stock(links : list, sleep_time : float) -> None:
     for link in links:
         storemap[link] = get_domain_name(link)
     print("Stock checking will now begin. To stop, close this window.\n")
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers = multiprocessing.cpu_count())
     # Stock Check Loop!
     while(True):
-        for link in links:
-            if is_in_stock(link, getDict(storemap[link])):
-                print(link + " Is in stock!!!")
+        pages = list(executor.map(fetch_content, links))
+        for i in range(len(pages)):
+            if is_in_stock(pages[i], get_relevant_dict(storemap[links[i]])):
+                print(links[i] + " Is in stock!!!")
                 playsound(soundpath)
         sleep(sleep_time)
 
 
 def remove_newlines(links : list) -> list:
-    """ Removes the newlines of the links that are given.
-
-    Arguments:
-        links: list[str]: The list of links, where each link is a string.
-    
-    Returns:
-        list[str]: The links with the newline characters removed.
-    """
     for i in range(len(links)):
         if links[i].endswith("\n"):
             links[i] = links[i][:-1]
     return links
 
 def get_domain_name(link : str) -> str:
-    """ Gets the domain name of a website."""
     return tldextract.extract(link).domain
 
-def getDict(domain : str) -> dict:
-    """ Gets the dictionary corresponding to the domain name. """
+def get_relevant_dict(domain : str) -> dict:
+    """ Gets the dictionary corresponding to the domain name.  \n
+    Requires: domain is one of \n
+    - "canadacomputers"
+    - "bestbuy"
+    - "newegg"
+    - "memoryexpress"
+    """
     return json.load(open(jsonfp + domain + ".json"))
 
-def is_in_stock(link : str, data : dict) -> bool:
+def fetch_content(link : str):
+    global browser_header
+    page = requests.get(link, headers=browser_header)
+    print("Got a page: " + link)
+    return page
+    
+def is_in_stock(page : requests.Response, data : dict) -> bool:
     """ Returns whether the item in link is in stock or not.
 
     Arguments:
@@ -80,8 +90,6 @@ def is_in_stock(link : str, data : dict) -> bool:
     
     Returns: bool
     """
-    global browser_header
-    page = requests.get(link, headers=browser_header)
     html = BeautifulSoup(page.content, 'html.parser')
     target_class : str = data["classCode"]
     messages : list = data["messages"] # list[str]
