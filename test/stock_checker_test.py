d36cc8750688d2dfe12d919f0025feea6a0de285
Rohit Pradhan <rohit.p.pradhan88@gmail.com>
2021-01-13 23:46:27 -0500

    Edited * imports to show which functions come from which files

--- test/stock_checker_test.py
+++ test/stock_checker_test.py
@@ -1,30 +1,30 @@
 ''' Testing the non-GUI elements of the stock checker. This includes the functions in
 stock_checker.py and html_parse.py. '''
-from testing_aid import *
-moveDirToSrcFolder()
-from stock_checker import *
+import testing_aid
+testing_aid.moveDirToSrcFolder()
+import stock_checker
+
 
 class TestStockCheck:
+
     # Checking remove_newlines
     def test_remove_newlines_one(self):
         normal = ["hello\n", "hi\n", "bonjour\n"]
-        assert remove_newlines(normal) == ["hello", "hi", "bonjour"]
-    
+        assert stock_checker.remove_newlines(normal) == ["hello", "hi", "bonjour"]
+
     def test_remove_newlines_emptylist(self):
         empty = []
-        assert remove_newlines(empty) == []
-    
+        assert stock_checker.remove_newlines(empty) == []
+
     def test_remove_newlines_no_newlines(self):
         no_newlines = ["this", "is", "a", "test"]
-        assert remove_newlines(no_newlines) == no_newlines
-    
-
+        assert stock_checker.remove_newlines(no_newlines) == no_newlines
 
     # Checking get_domain_name (gdn)
     def test_gdn_one(self):
         domain = "http://google.com"
-        assert get_domain_name(domain) == "google"
-    
+        assert stock_checker.get_domain_name(domain) == "google"
+
     def test_gdn_two(self):
-        domain="youtube.com"
-        assert get_domain_name(domain) == "youtube"
+        domain = "youtube.com"
+        assert stock_checker.get_domain_name(domain) == "youtube"
